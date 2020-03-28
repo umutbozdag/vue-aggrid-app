a47a99d83ba311424c4a2aa946b157537c35f717
Umut <bozdag321@hotmail.com>
2020-03-28 15:17:16 +0300

    feat: add modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="myModal" class="modal">
+    <!-- Modal content -->
+    <div class="modal-content">
+      <span @click="hideModal()" class="close">&times;</span>
+      <p>Some text in the Modal..</p>
+      <apexchart width="500" type="bar" :options="options" :series="series"></apexchart>
+    </div>
+  </div>
+</template>
+
+<script>
+import VueApexCharts from "vue-apexcharts";
+
+export default {
+  name: "Modal",
+  components: {
+    apexchart: VueApexCharts
+  },
+
+  data() {
+    return {
+      options: {
+        chart: {
+          id: "vuechart-example"
+        },
+        xaxis: {
+          categories: ["JAN", "FEB"]
+        }
+      },
+      series: [
+        {
+          name: "series-1",
+          data: [30, 40, 45, 50, 49, 60, 70, 91]
+        }
+      ]
+    };
+  },
+  methods: {
+    hideModal() {}
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.modal {
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0, 0, 0); /* Fallback color */
+  background-color: rgba(0, 0, 0, 0.4); /* Black w/ opacity */
+}
+
+/* Modal Content/Box */
+.modal-content {
+  background-color: #fefefe;
+  margin: 15% auto; /* 15% from the top and centered */
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%; /* Could be more or less, depending on screen size */
+}
+
+/* The Close Button */
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
+</style>
