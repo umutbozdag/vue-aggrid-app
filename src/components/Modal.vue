f338648195297c0034848e61907177d8932d2828
Umut <bozdag321@hotmail.com>
2020-03-28 19:09:36 +0300

    style: style

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,75 +1,98 @@
 <template>
-  <div id="myModal" class="modal">
-    <!-- Modal content -->
-    <div class="modal-content">
-      <span @click="hideModal()" class="close">&times;</span>
-      <p>Some text in the Modal..</p>
-      <apexchart width="500" type="bar" :options="options" :series="series"></apexchart>
+  <div>
+    <div v-if="openModal" id="myModal" class="modal">
+      <div class="modal-content">
+        <h2>Search Volume</h2>
+        <p>{{keyword}}</p>
+        <span @click="closeModal()" class="close">&times;</span>
+
+        <chart :volumeData="volumeData"></chart>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import VueApexCharts from "vue-apexcharts";
+import { bus } from "../main";
+import axios from "axios";
+import Chart from "./Chart";
 
 export default {
   name: "Modal",
   components: {
-    apexchart: VueApexCharts
+    Chart
+  },
+
+  created() {
+    bus.$on("open-modal", data => {
+      this.keyword = data.data.keyword;
+      this.openModal = true;
+      axios
+        .post("http://95.217.76.23:5454/api/get_specific_search_volume", {
+          keyword: data.data.keyword,
+          country: "tr",
+          lang: "tr"
+        })
+        .then(result => {
+          this.volumeData = result.data;
+          console.log(result);
+        });
+    });
   },
 
   data() {
     return {
-      options: {
-        chart: {
-          id: "vuechart-example"
-        },
-        xaxis: {
-          categories: ["JAN", "FEB"]
-        }
-      },
-      series: [
-        {
-          name: "series-1",
-          data: [30, 40, 45, 50, 49, 60, 70, 91]
-        }
-      ]
+      openModal: false,
+      volumeData: [],
+      keyword: ""
     };
   },
   methods: {
-    hideModal() {}
+    closeModal() {
+      this.openModal = false;
+    }
   }
 };
 </script>
 
 <style lang="scss" scoped>
 .modal {
-  position: fixed; /* Stay in place */
-  z-index: 1; /* Sit on top */
+  display: flex;
+  justify-content: center;
+  position: fixed;
+  z-index: 99;
   left: 0;
+  bottom: 0;
+  right: 0;
   top: 0;
-  width: 100%; /* Full width */
-  height: 100%; /* Full height */
-  overflow: auto; /* Enable scroll if needed */
-  background-color: rgb(0, 0, 0); /* Fallback color */
-  background-color: rgba(0, 0, 0, 0.4); /* Black w/ opacity */
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgb(0, 0, 0);
+  background-color: rgba(0, 0, 0, 0.5);
 }
 
-/* Modal Content/Box */
 .modal-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
   background-color: #fefefe;
   margin: 15% auto; /* 15% from the top and centered */
-  padding: 20px;
-  border: 1px solid #888;
-  width: 80%; /* Could be more or less, depending on screen size */
+  padding: 12px 24px;
+  border: 1px solid rgb(12, 12, 12);
+  width: 55%; /* Could be more or less, depending on screen size */
 }
 
-/* The Close Button */
 .close {
   color: #aaa;
   float: right;
-  font-size: 28px;
+  font-size: 48px;
   font-weight: bold;
+  position: absolute;
+  top: 0;
+  right: 0;
+  margin-right: 24px;
 }
 
 .close:hover,
