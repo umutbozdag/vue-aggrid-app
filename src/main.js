dafd9eaca5cfdb699a41283ac9ee702e42e2cf0d
Umut <bozdag321@hotmail.com>
2020-03-28 19:08:50 +0300

    feat: add event bus

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+export const bus = new Vue();
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
