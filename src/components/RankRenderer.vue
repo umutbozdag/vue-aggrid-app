755d6b16c07f38243c7b633206a5a11d5245bd6d
Umut <bozdag321@hotmail.com>
2020-03-28 19:10:02 +0300

    style: style

--- src/components/RankRenderer.vue
+++ src/components/RankRenderer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="cell-with-icon">
     {{this.params.value}}
-    <img class="star-icon" width="20px" src="../assets/star.svg" />
+    <img class="star-icon" width="18px" src="../assets/star.svg" />
   </div>
 </template>
 
@@ -12,4 +12,16 @@ export default Vue.extend({});
 </script>
 
 <style lang="scss" scoped>
+.cell-with-icon {
+  width: 50px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+  text-align: center;
+}
+
+.star-icon {
+  margin: 0 12px;
+}
 </style>
