78644f981c819d4b6d907e3ddd29ca5465cae680
Umut <bozdag321@hotmail.com>
2020-03-29 17:16:22 +0300

    feat: update new component

---
+++ src/components/PixelRankCell.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>{{params.value}}</div>
+</template>
+<script>
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style lang="scss" scoped>
+div {
+  width: 100px;
+  text-align: center;
+}
+</style>
