a9ca4bcb52451bf5ab99cd2c180fe05d47a464df
Umut <bozdag321@hotmail.com>
2020-03-29 15:21:45 +0300

    refactor: refactor

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
 <template>
   <div class="home">
     <h1>Zeo Interview</h1>
-    <div class="home__container">
+    <div class="home-container">
       <grid />
     </div>
     <modal />
@@ -27,12 +27,12 @@ export default {
 
 <style lang="scss" scoped>
 .home {
+  border-radius: 6px;
+  height: 100%;
+  margin: 0;
   h1 {
     margin-left: 24px;
     padding-top: 24px;
   }
-  border-radius: 6px;
-  height: 100%;
-  margin: 0;
 }
 </style>
